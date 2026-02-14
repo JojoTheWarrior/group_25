5ac3b0bf5b3fecfd3091f4407d82b3bd7229fb96
JojoTheWarrior <joshua.wang828@gmail.com>
2026-02-14 14:43:39 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Stack
+- Set up a Django endpoint that you can ping
+- Use multiprocessing in Python to simultaneously iterate over different flush selections
+- Have some data structure of "all valid histograms"
+- Make a website where you can play the game
+
+# Algorithm
+- Precompute all valid histograms and store them into a tree
+- Enumerate over flush picks, and travel the tree for each remaining set to see if it works
---
+++ valid_histograms.py
@@ -0,0 +1,27 @@
+
+# list of tuples that look like (type, a, b), 0 <= a, b <= 12
+# if type = 0, it's a straight that starts on a
+# if type = 1, it's a full house with 3 on a and 2 on b
+# if type = 2, it's a 4oak with 4 on a and 1 on b
+pieces = []
+
+for i in range(0, 9):
+    pieces.append((0, i, 0))
+
+for a in range(0, 13):
+    for b in range(0, 13):
+        if a == b:
+            continue
+        pieces.append((1, a, b))
+        pieces.append((2, a, b))
+
+choose = [[0 for i in range(6)] for _ in range(322)]
+for n in range(322):
+    choose[n][0] = 1
+    for k in range(1, 6):
+        if k > n:
+            continue
+        choose[n][k] = choose[n-1][k-1] + choose[n-1][k]
+
+def id_to_selection(num, id):
+    pass
