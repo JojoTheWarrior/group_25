902b5348ba1722531b7173df5257bc74de91b179
JojoTheWarrior <joshua.wang828@gmail.com>
2026-02-15 15:00:09 -0500

    375M lineups reached

--- valid_histograms.py
+++ valid_histograms.py
@@ -23,5 +23,48 @@ for n in range(322):
             continue
         choose[n][k] = choose[n-1][k-1] + choose[n-1][k]
 
-def id_to_selection(num, id):
+completed = 0
+
+def tree_search(rem: int, lineup: list[int]) -> None:
+    global completed
+    if rem == 0: # found a valid selection
+        if completed % 100000 == 0:
+            print(completed)
+        completed += 1
+        return
     
+    for piece in pieces:
+        piece_type, a, b = piece
+        # straights
+        if piece_type == 0:
+            for i in range(a, a+5):
+                lineup[i] += 1
+            flag = True
+            for i in range(a, a+5):
+                if lineup[i] > 4:
+                    flag = False
+            if flag:
+                tree_search(rem-1, lineup)
+            for i in range(a, a+5):
+                lineup[i] -= 1
+        # full houses
+        if piece_type == 1:
+            lineup[a] += 3
+            lineup[b] += 2
+            flag = (lineup[a] <= 4 and lineup[b] <= 4)
+            if flag:
+                tree_search(rem-1, lineup)
+            lineup[a] -= 3
+            lineup[b] -= 2
+        # 4oak
+        if piece_type == 2:
+            lineup[a] += 4
+            lineup[b] += 1
+            flag = (lineup[a] <= 4 and lineup[b] <= 4)
+            if flag:
+                tree_search(rem-1, lineup)
+            lineup[a] -= 4
+            lineup[b] -= 1
+
+blank_lineup = [0 for i in range(13)]
+tree_search(5, blank_lineup)
